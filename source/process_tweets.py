68f52a415fe041e9b9868dbe1f31aaa53d0dc1fe
DiklaGelbard <dikla.gelbard@gmail.com>
2018-09-16 03:35:44 +0300

    process tweets

--- source/process_tweets.py
+++ source/process_tweets.py
@@ -12,9 +12,8 @@ MAX_BOW=40
 MAX_WRD2VEC=10
 
 
-def process_text(txt):
+def process_train_text(txt):
     tknzr = TweetTokenizer()
-    #txt = [ tknzr.tokenize(wrd) for wrd in txt ]
     sentiments = []
     for wrd in txt:
         sentiments.append(TextBlob(wrd).sentiment)
@@ -26,12 +25,23 @@ def process_text(txt):
     return scipy.sparse.hstack([txt, np.array(sentiments)])
 
 
-def get_text_features(textual_data):
-    # word2vec for usernames
-    #author_emb = process_author_name(textual_data[:,1])
+def process_test_text(txt):
+    sentiments = []
+    for wrd in txt:
+        sentiments.append(TextBlob(wrd).sentiment)
+    vectorizer = pkl.load("../data/vectorizer.pkl",'rb')
+    txt = vectorizer.transform(txt)
+    return scipy.sparse.hstack([txt, np.array(sentiments)])
+
+
+def get_text_features(textual_data,isTrain):
     # features for tweet body
     total = textual_data[:,0] + textual_data[:,1]
-    txt_emb = process_text(total)
+    if isTrain:
+        txt_emb = process_train_text(total)
+    else:
+        txt_emb = process_test_text(total)
+
     # text_features = scipy.sparse.hstack([author_emb, txt_emb])
     return txt_emb
 
@@ -39,11 +49,11 @@ def get_text_features(textual_data):
 COL_NUM_NAMES = ["retweet_count","user_verified","user_friends_count","user_followers_count","user_favourites_count","num_hashtags","num_mentions"]
 
 
-def get_features(tweet_data):
+def get_features(tweet_data,isTrain=False):
     numeric_data = tweet_data[COL_NUM_NAMES]
     numeric_data["user_verified"] = numeric_data["user_verified"].astype(int)
     textual_data = tweet_data[['text', 'user_screen_name']].values
-    textual_data = get_text_features(textual_data)
+    textual_data = get_text_features(textual_data,isTrain)
     return np.hstack([np.asarray(scipy.sparse.csr_matrix.todense(textual_data)),numeric_data.values])
 
 
@@ -51,7 +61,7 @@ def get_features(tweet_data):
 def get_train_data(path="../data/electionday_tweets_clean.xlsx"):
     original_data = pd.read_excel(path, header=0)
     labels = original_data["is_fake_news"].astype(int).values
-    processed_data = get_features(original_data[['text', 'user_screen_name'] + COL_NUM_NAMES])
+    processed_data = get_features(original_data[['text', 'user_screen_name'] + COL_NUM_NAMES],True)
     return processed_data, labels
 
 
