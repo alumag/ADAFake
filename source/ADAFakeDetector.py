c331958a06854f9f25e41689c4d8c58bd52374d6
Aluma Gelbard <violetaluma@hotmail.com>
2018-09-16 04:21:26 +0300

    typo

--- source/ADAFakeDetector.py
+++ source/ADAFakeDetector.py
@@ -1,12 +1,12 @@
-import pandas as pd
+import pandas
 from .model import evaluate
-from .process_tweets import get_features,COL_NUM_NAMES
+from .process_tweets import get_features, COL_NUM_NAMES
 
 
 def check_tweet(tweet_data):
     """"
     :return True on fake news, false on true once
     """
-    tweet_data = pd.dataframe(tweet_data)
+    tweet_data = pandas.DataFrame(tweet_data)
     processed_data = get_features(tweet_data[['text', 'user_screen_name'] + COL_NUM_NAMES])
     return evaluate(processed_data)
