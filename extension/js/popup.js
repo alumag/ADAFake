48b7c587a0d5dc3abb357b1146415b923f6172e1
Aluma Gelbard <violetaluma@hotmail.com>
2018-09-16 06:13:36 +0300

    represent the percentage nicely

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -25,8 +25,8 @@ function CheckTweet(tab) {
                 stats.innerHTML += " is completely legit AFFFF ".bold().fontcolor("green");
                 stats.innerHTML += "<img src=\"/icons/legit.jpg\"/>";
             }
-            stats.innerHTML += "</br>" + data["fake_news"] + "%";
-
+            stats.innerHTML += "<h4>Chances of Fake-News (%) </h4>";
+            add_bar(data["fake_news"]);
          }
     };
     xhttp.open("GET", server + "?url=" + url, true);
@@ -55,3 +55,36 @@ window.onload = function() {
         }
     });
 };
+
+function add_bar(percent) {
+  var data = [1 * 100, percent * 100]; // here are the data values; v1 = total, v2 = current value
+
+  var chart = d3.select("#container").append("svg") // creating the svg object inside the container div
+    .attr("class", "chart")
+    .attr("width", 200) // bar has a fixed width
+    .attr("height", 20 * data.length);
+
+  var x = d3.scale.linear() // takes the fixed width and creates the percentage from the data values
+    .domain([0, d3.max(data)])
+    .range([0, 200]);
+
+  chart.selectAll("rect") // this is what actually creates the bars
+    .data(data)
+  .enter().append("rect")
+    .attr("width", x)
+    .attr("height", 20)
+    .attr("rx", 5) // rounded corners
+    .attr("ry", 5);
+
+  chart.selectAll("text") // adding the text labels to the bar
+    .data(data)
+  .enter().append("text")
+    .attr("x", x)
+    .attr("y", 10) // y position of the text inside bar
+    .attr("dx", -3) // padding-right
+    .attr("dy", ".35em") // vertical-align: middle
+    .attr("text-anchor", "end") // text-align: right
+    .text(String);
+
+   document.body.appendChild(x);
+}
